9a6f753b130c5070a39f075d18915baa20493dcd
gianogli <gianogli@tiscali.it>
2016-12-31 18:42:30 +0100

    Update TOOLCHAIN version to 4.9

--- bbuild-anykernel.sh
+++ bbuild-anykernel.sh
@@ -12,7 +12,7 @@
 
 BOEFFLA_VERSION="6.0-beta4-CM14.1-i9300-UNOFFICIAL"
 
-TOOLCHAIN="/home/gianogli/dati/android/my_toolchain/arm-eabi-4.8/bin/arm-eabi-"
+TOOLCHAIN="/home/gianogli/dati/android/my_toolchain/arm-eabi-4.9/bin/arm-eabi-"
 ARCHITECTURE=arm
 COMPILER_FLAGS_KERNEL="-mtune=cortex-a9 -fno-diagnostics-show-caret"
 COMPILER_FLAGS_MODULE="-mtune=cortex-a9 -fno-diagnostics-show-caret"
